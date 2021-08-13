24fa49d33f3ffb1c9858301f571b2c37ef33d866
Ted Verdonkschot <tedverdonkschot@outlook.com>
2021-08-13 11:50:53 -0700

    Add display book function

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 // Store books
-let myLibrary = [{
-    title: 'Crime and Punishment',
-    author: 'Fyodor Dostoyevsky',
-    pages: 522,
-    status: 'Incomplete'
-},
-{
-    title: 'Candide',
-    author: 'Voltaire',
-    pages: 130,
-    status: 'Incomplete'
-}];
+let myLibrary = ["Crime and Punishment by Dostoyevsky, 522 p., Incomplete", 
+"Candide by Voltaire, 130 p., Incomplete", 
+"Fear and Trembling, SK, 150 p., Incomplete"
+];
+
 
 // Book constructor
 function Book(title, author, pages, status) {
@@ -29,9 +22,16 @@ function addBookToLibrary() {
 }
 
 function displayBooks() {
-    
+    const container = document.querySelector('.container');
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const div = document.createElement('div');
+        div.className = "book";
+        div.textContent = myLibrary[i];
+        container.appendChild(div);
+    }
 }
 
-// get container and store in var, create a var for each card (a div),
-// for each book, append a div to container, make the text content the book info
+displayBooks();
+
 
