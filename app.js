7ca78edf3b7fee66d730d1e6b25f671556ab308f
Ted Verdonkschot <tedverdonkschot@outlook.com>
2021-08-16 13:14:44 -0700

    Add functionality to status button

--- app.js
+++ app.js
@@ -13,36 +13,58 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+// Add book cards to DOM, delete button and completion toggle included
 function displayBooks() {
     const container = document.querySelector('.container');
     while (container.firstChild) {
         container.removeChild(container.lastChild)
-    }; 
+    };
 
     for (let i = 0; i < myLibrary.length; i++) {
+        // Book div card
         const div = document.createElement('div');
-        const deleteBtn = document.createElement('button');
+        div.className = "book";
+        div.dataset.index = i;
+        div.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].status}`;
+
+        // Options div
+        const options = document.createElement('div');
+        options.className = "book-options"
+
+        // Book status button
+        const statusBtn = document.createElement('button');
+        statusBtn.className = "status-button";
+        statusBtn.textContent = "Change Status"
+        statusBtn.dataset.index = i;
+
+        statusBtn.addEventListener("click", function updateStatus() {
+            if (myLibrary[statusBtn.dataset.index].status == "Finished") {
+                myLibrary[statusBtn.dataset.index].status = "Unfinished";
+            } else {
+                myLibrary[statusBtn.dataset.index].status = "Finished";
+            }
+            displayBooks();
+        });
         
+        // Book delete button
+        const deleteBtn = document.createElement('button');
         deleteBtn.className = "delete-button";
-        deleteBtn.textContent = "X"
+        deleteBtn.textContent = "Delete"
         deleteBtn.dataset.index = i;
         deleteBtn.addEventListener("click", function deleteBook() {
-            if (deleteBtn.parentElement.dataset.index == this.dataset.index) {
+            if (deleteBtn.parentElement.parentElement.dataset.index == this.dataset.index) {
                 // Remove book div from DOM
-                this.parentElement.parentElement.removeChild(this.parentElement);
+                this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement);
                 // Remove book object from library
                 myLibrary.splice(this.dataset.index, 1);
                 console.log(myLibrary)
             }
         })
 
-
-        div.className = "book";
-        div.dataset.index = i; 
-        div.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].status}`;
-        
         container.appendChild(div);
-        div.appendChild(deleteBtn);
+        options.appendChild(statusBtn);
+        options.appendChild(deleteBtn);
+        div.appendChild(options);
     }
     clearFields();
 }
@@ -72,12 +94,3 @@ function clearFields() {
     document.querySelector('#bookTitle').value = '';
     document.querySelector('#author').value = '';
 }
-
-// delete book
-/*var deleteButton = document.querySelector(".delete-button");
-
-deleteButton.addEventListener("click", function deleteBook() {
-    if (deleteButton.parentElement.dataset.index == this.dataset.index) {
-        this.parentElement.parentElement.removeChild(this.parentElement);
-    }
-})*/
